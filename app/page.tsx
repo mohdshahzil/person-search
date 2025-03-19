6585c1a12a7555cf80204daec78f24573c76eae0
mohdshahzil <theshahzil@gmail.com>
2025-03-19 06:04:32 +0530

    dummy comment

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { UserDialog } from './components/user-dialog';
 export default async function Home({ searchParams }: { searchParams: Promise<{ userId?: string }> }) {
   return (
     <div className="container mx-auto px-4 py-8">
+      {/* comment added */}
       <h1 className="text-3xl font-bold mb-8">User Search</h1>
       <UserSearch searchParams={searchParams} />
       <UserDialog />
